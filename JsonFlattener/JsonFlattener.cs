7944c225df8b9241ec795d7ae9c59a6d3687ae07
Wojtek Dziegielewski <wojtekd@mavidian.com>
2020-11-25 22:13:43 -0500

    Implement JSON flattening logic.

--- JsonFlattener/JsonFlattener.cs
+++ JsonFlattener/JsonFlattener.cs
@@ -1,8 +1,147 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace JsonFlattener
 {
-   public class JsonFlattener
+   public class JsonFlattener : IDisposable
    {
+      private readonly JsonTextReader _jsonReader;
+
+      /// <summary>
+      /// Label or Counter: "Either" type; type of elements in stack of prefixes.
+      /// JSON object is represented on _keyPrefixes stack by Label; JSON array is represented by Label + Counter
+      /// </summary>
+      private class LorC
+      {
+         private string _label;
+         private int _counter;
+
+         public LorC(string label) { _label = label; }  // creates Label
+         public LorC() { _label = null; _counter = 1; }  // creates Counter (1-based)
+
+         public bool IsCounter => _label == null;
+         public bool IsLabel => !IsCounter;
+
+         public string Value => IsLabel ? _label : $"{_counter:D2}";
+
+         public void Increment() => _counter++;
+      }
+
+      private readonly Stack<LorC> _keyPrefixes;  // elements to build column name from
+      private const string _pfxSep = "_";  // separates prefixes, i.e. segments of column nams
+      private string _LPN;  // last PropertyName
+      private readonly LorC _emptyLabel = new LorC(string.Empty);  // part to ignore when building columns name from _keyPrefixes
+
+      public JsonFlattener(TextReader textReader)
+      {
+         _jsonReader = new JsonTextReader(textReader);
+         _keyPrefixes = new Stack<LorC>();
+         _keyPrefixes.Push(_emptyLabel);
+         _LPN = string.Empty;
+         // If JSON contains top-level array (i.e. [...]), then compound column names will start at numbers; to prevent this
+         // _LPN can be assinged here a value, such as "ROOT".
+      }
+
+
+      /// <summary>
+      /// Sequence of tokens obtained from JSON input (useful for testing/exploration purposes).
+      /// </summary>
+      /// <returns></returns>
+      public IEnumerable<(JsonToken Type, object Value)> GetJsonTokenData()
+      {
+         while (_jsonReader.Read())
+         {
+            yield return (_jsonReader.TokenType, _jsonReader.Value?.ToString());
+         }
+      }
+
+
+      /// <summary>
+      /// Sequence of flattened key-value pairs obtained from JSON input.
+      /// Key reflects the path to JSON element and uniquely defines each value.
+      /// </summary>
+      /// <returns></returns>
+      public IEnumerable<KeyValuePair<string, object>> GetFlattenedData()
+      {
+         while (_jsonReader.Read())
+         {
+            var tokenType = _jsonReader.TokenType;
+            var tokenValue = _jsonReader.Value;
+
+            switch (tokenType)
+            {
+               case JsonToken.StartArray:
+                  PushLPNorEmptyLabelIfCounterOnTop();
+                  _keyPrefixes.Push(new LorC()); //counter
+                  break;
+               case JsonToken.StartObject:
+                  PushLPNorEmptyLabelIfCounterOnTop();
+                  break;
+               case JsonToken.PropertyName:
+                  _LPN = (string)tokenValue;  // remember last PropertyName value for use in the next token
+                  break;
+               case JsonToken.String:
+               case JsonToken.Integer:
+               case JsonToken.Float:
+               case JsonToken.Boolean:
+               case JsonToken.Null:
+               case JsonToken.Undefined:  // absence of value, e.g. "MyUndefinedValue":, - technically invalid JSON, but Json.NET parses it as Undefined
+                  yield return new KeyValuePair<string, object>(GetColumnName(), tokenValue);
+                  IncrementCounterIfAnyOnTop();
+                  break;
+               case JsonToken.EndObject:
+                  Debug.Assert(_keyPrefixes.Peek().IsLabel);
+                  _keyPrefixes.Pop();
+                  IncrementCounterIfAnyOnTop();
+                  break;
+               case JsonToken.EndArray:
+                  Debug.Assert(_keyPrefixes.Peek().IsCounter);
+                  _keyPrefixes.Pop();
+                  Debug.Assert(_keyPrefixes.Peek().IsLabel);
+                  _keyPrefixes.Pop();
+                  //TODO: ?? increment ??
+                  break;
+               default:
+                  throw new InvalidDataException($"Unexpected JSON '{tokenType}' token with a value '{tokenValue}'.");
+            }
+         }
+         yield break;
+      }
+
+
+      private void PushLPNorEmptyLabelIfCounterOnTop()
+      {
+         //Counter on top means that array is the last element on _keyPrefixes stack. If so, the current element will not become part of the Key - note that
+         // array elements are not named. However,  an emptyLabel element still needs to be added to properly track object/array nesting.
+         _keyPrefixes.Push(_keyPrefixes.Peek().IsLabel ? new LorC(_LPN) : _emptyLabel);
+      }
+
+
+      private string GetColumnName()
+      {
+         // Empty labels are elememnts on the _keyPrefixes stack that are not part of the key hierarchy (such as those elements that immediately follow counters, i.e. JSON arrays).
+         return string.Join(_pfxSep, _keyPrefixes.Reverse()
+                                              .Append(_keyPrefixes.Peek().IsLabel ? new LorC(_LPN) : _emptyLabel)
+                                              .Select(p => p.Value)
+                                              .Where(v => v != string.Empty));
+      }
+
+
+      private void IncrementCounterIfAnyOnTop()
+      {
+         var topPfx = _keyPrefixes.Peek();
+         if (topPfx.IsCounter) topPfx.Increment();
+      }
+
+      public void Dispose()
+      {
+         ((IDisposable)_jsonReader).Dispose();
+      }
+
+
    }
 }
--- JsonFlattener_tester/Program.cs
+++ JsonFlattener_tester/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace JsonFlattener_tester
 {
    class Program
    {
+      private static readonly char sep = Path.DirectorySeparatorChar;
+
+      private static readonly string _data = "TestData";
+      //private static readonly string _data = "SampleMedicalEventData";
+
+      private static readonly bool _outputToFile = false;  // if false, detailed JSON tokens are displayed one by one on the console window
+
+ 
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello World!");
+         var jsonInput = $"..{sep}..{sep}..{sep}Data{sep}{_data}.json";
+         var kvpOutput = $"{_data}.kvp";
+
+         Console.WriteLine($"JSON input file: {Path.GetFullPath(jsonInput)}");
+         if (_outputToFile) Console.WriteLine($"KVP output file: {Path.GetFullPath(kvpOutput)}");
+
+         using (var textReader = File.OpenText(jsonInput))
+         using (var flattener = new JsonFlattener.JsonFlattener(textReader))
+         using (var textWriter = File.CreateText(kvpOutput))
+         {
+
+            if (_outputToFile)
+            {
+               foreach (var kvp in flattener.GetFlattenedData())
+               {
+                  textWriter.WriteLine($"Key={kvp.Key}  Value={kvp.Value}");
+               }
+            }
+            else // step through generated JSON tokens
+            {
+               Console.WriteLine("Press a key to obtain next token..");
+               foreach (var token in flattener.GetJsonTokenData())
+               {
+                  Console.ReadKey(true);
+                  Console.WriteLine($"  {token.Type,-16}{(token.Value == null ? string.Empty : token.Value)}");
+               }
+               Console.WriteLine("End of JSON data.");
+            }
+         }
+
+         if (Debugger.IsAttached)
+         {
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey();
+         }
+
       }
    }
 }
